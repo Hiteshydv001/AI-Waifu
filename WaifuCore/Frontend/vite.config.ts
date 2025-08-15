75c66b01ae18f707298eb864ad1db40b1818e2ae
HITESH KUMAR <114931638+Hiteshydv001@users.noreply.github.com>
2025-08-15 12:56:28 +0530

    adding final

--- WaifuCore/Frontend/vite.config.ts
+++ WaifuCore/Frontend/vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@/lib": path.resolve(__dirname, "./src/lib"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/hooks": path.resolve(__dirname, "./src/hooks"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/config": path.resolve(__dirname, "./src/config")
     },
   },
   build: {
