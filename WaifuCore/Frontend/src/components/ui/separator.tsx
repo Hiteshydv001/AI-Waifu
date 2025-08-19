f60521ef93bf9e02f033da7a11750a54dd0194f6
HITESH KUMAR <114931638+Hiteshydv001@users.noreply.github.com>
2025-08-19 12:48:27 +0530

    starting again

--- WaifuCore/Frontend/src/components/ui/separator.tsx
+++ WaifuCore/Frontend/src/components/ui/separator.tsx
@@ -1,8 +1,7 @@
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
-import utils from "../../lib/utils"
-const { cn } = utils
+import { cn } from "../../lib/utils"
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
