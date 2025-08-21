3e7ddb82d6bbce9b985492979ecc79e0ea5e0555
HITESH KUMAR <114931638+Hiteshydv001@users.noreply.github.com>
2025-08-21 23:46:55 +0530

    fix deploy

--- WaifuCore/Frontend/vite.config.ts
+++ WaifuCore/Frontend/vite.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, 'src'),
+      '@/lib': path.resolve(__dirname, 'src/lib'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/lib/utils': path.resolve(__dirname, 'src/lib/utils.ts')
     }
   },
   build: {
